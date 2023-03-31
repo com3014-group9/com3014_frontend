394d0e8c2fb3aac9699303e46f764f0867604371
Jack Pennington <jp01186@surrey.ac.uk>
2023-03-31 22:15:54 +0100

    added some cat pictures :)

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,11 +2,19 @@
     <div class="home">
       <h1>Welcome to the Home Page</h1>
       <p>This is an example of the home page content</p>
+      <img src="public\tilly.jpg" />
+      <img src="public\tilly2.jpg" />
+      <img src="public\tilly3.jpg" />
+      <img src="public\tilly4.jpg" />
+      <img src="public\tilly5.jpg" />
+      <img src="public\tilly6.jpg" />
+      <img src="public\tilly7.jpg" />
     </div>
   </template>
   
   <script>
   export default {
+
     name: "home",
     data() {
       return {
@@ -15,3 +23,11 @@
     },
   };
   </script>
+
+<style>
+  img {
+   max-width: 50%;
+   max-height: 50%;
+   padding: 2rem;
+     }
+</style>
